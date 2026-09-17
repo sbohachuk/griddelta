44e39d85d97e61749098a341ce3c630953520e41
sbohachuk <bogachuk.sasha1@gmail.com>
2026-09-17 12:42:07 +0300

    feat: native React Import/Export page with embedded Sheets snapshot

--- src/routes/import-export.tsx
+++ src/routes/import-export.tsx
@@ -1,22 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
+import ImportExportPage from "@/components/ImportExportPage";
 
 export const Route = createFileRoute("/import-export")({
   ssr: false,
   component: ImportExportPage,
 });
-
-function ImportExportPage() {
-  return (
-    <iframe
-      title="Імпорт / Експорт"
-      src="/import-export.html"
-      style={{
-        border: "none",
-        width: "100%",
-        height: "100dvh",
-        display: "block",
-        background: "#0b1220",
-      }}
-    />
-  );
-}
